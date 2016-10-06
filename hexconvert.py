86272060d803dfba528051b8e175cf687604008e
Leslie_Leung <lesily9@gmail,com>
2016-10-06 19:47:10 +0800

    a hex converter

---
+++ hexconvert.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Author:Leslie_Leung
+The programme is to convert a number into hex
+'''
+print("The programme is to convert a number into hex")
+n=int(input('Please input the number you want to convert\n'))
+print(hex(n))
